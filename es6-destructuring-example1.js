36e79dab2009a37ac16b09e006b366a445b82c9c
Hamza Sheikh <hsheikhm@gmail.com>
2016-11-01 19:11:19 +0000

    created first example for destructuring

---
+++ es6-destructuring-example1.js
@@ -0,0 +1,44 @@
+/*jshint esversion: 6 */
+
+// ES5:
+
+function foo(options) {
+  var count = options.count;
+  var msg = options.msg;
+
+  for(var i = 0; i < count; i++) {
+    console.log((i + 1) + '. ' + msg);
+  }
+}
+
+foo({count: 3, msg: 'something', junk: 88});
+// 1. something
+// 2. something
+// 3. something
+
+// #############################################################################
+
+// ES6:
+
+function foo(options) {
+  let {count, msg} = options;
+
+  for(var i = 0; i < count; i++) {
+    console.log((i + 1) + '. ' + msg);
+  }
+}
+
+// OR:
+
+function foo(options) {
+  let {count: max, msg} = options;
+
+  for(var i = 0; i < max; i++) {
+    console.log((i + 1) + '. ' + msg);
+  }
+}
+
+foo({count: 3, msg: 'something', junk: 88});
+// 1. something
+// 2. something
+// 3. something
