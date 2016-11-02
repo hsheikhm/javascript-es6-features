70e75cc29250760da178ed98c2f5c14b6b5798f5
Hamza Sheikh <hsheikhm@gmail.com>
2016-11-02 19:45:57 +0000

    created example for promises

---
+++ es6-promises.js
@@ -0,0 +1,15 @@
+/*jshint esversion: 6 */
+
+// ES6:
+
+function asyncFn(foo) {
+  return new Promise((resolve, reject) => {
+    if(foo === 7) {
+      return resolve('lucky winner');
+    }
+
+    return reject('oh too bad!');
+  });
+}
+
+asyncFn(7).then(success => console.log(success), failure => console.log(failure));
